5bfd7f3cba3dee3730d6d71a42de108770c42fd9
Abhishek <pundirabhishek987@gmail.com>
2024-01-21 07:59:41 +0530

    configure : tailwindCSS

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Namaste Everyone!</h1>
+      <p className="text-xl font-bold underline">Namaste Everyone!</p>
     </div>
   );
 }
