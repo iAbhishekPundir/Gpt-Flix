d8bdf7ca920f0400b1ba9498eeba11177a08618a
Abhishek <pundirabhishek987@gmail.com>
2024-01-21 07:08:57 +0530

    refactor code

--- netflix-gpt/src/App.js
+++ netflix-gpt/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Namaste Everone!</h1>
+      <h1>Namaste Everyone!</h1>
     </div>
   );
 }
